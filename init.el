6d9bbea843309957190d12a574d3471bcaa3ed33
Gabe Johnson <gabe.johnson@gmail.com>
2013-01-13 17:37:25 -0700

    Added initial emacs stuff
    Signed-off-by: Gabe Johnson <gabe.johnson@gmail.com>

---
+++ init.el
@@ -0,0 +1,40 @@
+;; Gabe Johnson's .emacs file, most of which was stolen, plundered,
+;; and plagarized from various people on the Intertron.
+;;
+;; An updated version may or may not be found at http://six11.org.
+
+;; Add some things to the load path
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(load-theme 'wombat t)
+(setq inhibit-startup-screen t)
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+(setq visible-bell t)
+
+;; uniquify: ensures two buffers are easiliy distinguished by
+;; appending contending buffers with their file directory. Eg. if you
+;; have /foo/views.py and /bar/views.py buffers open, the buffers will
+;; be renamed views.py:foo and views.py:bar. Happy.
+(require 'uniquify) 
+(setq 
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator ":")
+
+(global-set-key "\M-c" 'copy-region-as-kill)
+(global-set-key "\M-v" 'yank)
+
+(setenv "PATH"
+	(concat
+	 "/usr/local/bin" ":"
+	 (getenv "PATH")))
