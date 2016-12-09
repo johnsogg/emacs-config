8601c63850c0fa42c3ab00914ab3880a209da4c3
Gabe Johnson <gabe@blankslatesystems.com>
2016-12-09 12:55:23 -0700

    Add my color theme, set tab space size for go mode.

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (setq inhibit-startup-screen t)     ; Disable startup screen with graphics
 (set-default-font "Monaco 12")      ; Set font and size
 (setq-default indent-tabs-mode nil) ; Use spaces instead of tabs
-(setq tab-width 2)                  ; Four spaces is a tab
+(setq tab-width 2)                  ; Two spaces is a tab
 (setq visible-bell nil)             ; Disable annoying visual bell graphic
 (setq ring-bell-function 'ignore)   ; Disable super annoying audio bell
 
@@ -25,10 +25,8 @@
 (global-set-key [f8] 'neotree-toggle) ; keybinding is global
 (setq neo-smart-open t)               ; find current file on open
 
-(load-theme 'atom-one-dark t)       ; Color theme installed via melpa
-(set-face-attribute
- 'highlight nil :background "#5EF763"
- :foreground "#444")
+;; Color theme
+(load-theme 'johnsogg-dark t)         ; My colors for optimal rad.
 
 ;; Add a directory to the load path so we can put extra files there
 (add-to-list 'load-path "~/.emacs.d/lisp/")
@@ -58,7 +56,9 @@
   (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
 
   ;; Misc go stuff
+  (setq tab-width 2)                              ; Somehow gets overwritten?
   (auto-complete-mode 1))                         ; Enable auto-complete mode
+
 
 ;; Connect go-mode-hook with the function we just defined
 (add-hook 'go-mode-hook 'my-go-mode-hook)
@@ -70,6 +70,26 @@
 ;; If the go-guru.el file is in the load path, this will load it.
 (require 'go-guru)
 
+;; Use reasonable keybindings to change font size for all buffers.
+(define-globalized-minor-mode 
+  global-text-scale-mode
+  text-scale-mode
+  (lambda () (text-scale-mode 1)))
+(defun global-text-scale-adjust (inc) (interactive)
+       (text-scale-set 1)
+       (kill-local-variable 'text-scale-mode-amount)
+       (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+       (global-text-scale-mode 1)
+       )
+(global-set-key (kbd "M-0")
+		'(lambda () (interactive)
+		   (global-text-scale-adjust (- text-scale-mode-amount))
+		   (global-text-scale-mode -1)))
+(global-set-key (kbd "M-=")
+		'(lambda () (interactive) (global-text-scale-adjust 1)))
+(global-set-key (kbd "M--")
+		'(lambda () (interactive) (global-text-scale-adjust -1)))
+
 ;; -------------- Everything below here is automatically added by programs -----
 
 (custom-set-variables
@@ -77,6 +97,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c4a236af4ed91fbb5dc3f10f954210ee4e2d6a524ab02ae4bdb957db7acb8b73" "d40327db0dbc8e2ca544348516d453057af851f24c0bf20b8e3eb94b8df6dc28" "19f2972c71c8877a6ab53c83b13ab6adc7822715d3de5dfa758e6fb1630bf326" "dae974861c4b76ed1209634e307e2e8a885eedb41e4035f8931e21f743f5c88f" "6254372d3ffe543979f21c4a4179cd819b808e5dd0f1787e2a2a647f5759c1d1" default)))
+ '(fci-rule-color "#3E4451")
  '(package-selected-packages
    (quote
     (yaml-mode flymake-go atom-one-dark-theme neotree go-autocomplete auto-complete exec-path-from-shell go-mode))))
